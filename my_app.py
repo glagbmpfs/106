1fdf88732349068aea5c1436d2fafa2396b3429c
glagbmpfs <91646069+glagbmpfs@users.noreply.github.com>
2022-05-08 16:16:29 +0900

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -16,6 +16,20 @@ class MainWin(QWidget):
         pass
     def show(self): 
         pass
+ def set_appear(self): 
+    self.setWindowTitle(txt_title)
+    self.resize(win_width, win_height)
+    self.move(win_x, win_y)
+
+
+def initUI(self):
+    self.hello_text = QLabel( txt_hello )
+    self.instruction = QLabel( txt_instruction )
+    self.button = QPushButton( txt_next )
+    self.layout = QVBoxLayout()
+    self.hello_text.addWidget(self.layout)
+    self.instruction.addWidget(self.layout)
+    self.button.addWidget(self.layout)
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
        QApplication, QWidget,
